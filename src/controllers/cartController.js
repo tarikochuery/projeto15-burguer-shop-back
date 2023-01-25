41295a07754ef06ea24adeb1432a1b1b7ef407f9
tarikochuery <ttarikechuery@gmail.com>
2023-01-25 20:55:19 -0300

    Refact: Altera busca por usuário utilizando userId em vez de email

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -5,13 +5,12 @@ import { ObjectId } from "bson";
 const cartController = {
   async addToCartById(req, res) {
     const { id } = req.params;
-    const { email } = req.headers;
+    const { userId } = res.locals;
 
     const product = await burguershopdb.collection(COLLECTIONS.products).findOne({ _id: ObjectId(id) });
-    console.log(product);
     if (!product) return res.status(404).send('Produto não encontrado');
 
-    const user = await burguershopdb.collection(COLLECTIONS.users).findOne({ email });
+    const user = await burguershopdb.collection(COLLECTIONS.users).findOne({ _id: ObjectId(userId) });
     if (!user) return res.sendStatus(401);
 
     const { cart } = user;
